8d1cdf8c16f3f20c1e4a855227ae56c81f5686a3
William Taylor <wi11berto@yahoo.co.uk>
2016-06-14 20:23:41 +0100

    Fixing indent in files

--- app/src/main/java/drawables/HelpObject.java
+++ app/src/main/java/drawables/HelpObject.java
@@ -6,120 +6,120 @@ import java.util.*;
 import objects.Globals;
 
 public class HelpObject implements IRenderable {
-	private ClickEvent gotchaClick;
-	private ClickEvent showClick;
-	private Button dontShowButton;
-	private Button gotchaButton;
-	private Image helpBackground;
-	private Boolean enabled;
+    private ClickEvent gotchaClick;
+    private ClickEvent showClick;
+    private Button dontShowButton;
+    private Button gotchaButton;
+    private Image helpBackground;
+    private Boolean enabled;
 
-	public HelpObject() {
-		Font font = Font.get("Tiny");
-		enabled = Globals.get().ShowHelp();
+    public HelpObject() {
+        Font font = Font.get("Tiny");
+        enabled = Globals.get().ShowHelp();
 
-		dontShowButton = new Button(font);
-		dontShowButton.setSprite("sprites/button3.png", 830, 100, 420, 80);
-		dontShowButton.setText("GOT IT !", 1030, 125, 300, 50);
-		dontShowButton.setTextColour(1, 1, 0, 1);
-		
-		helpBackground = new Image("sprites/help.bmp");
-		helpBackground.setPosition(0, 0, 1280, 750);
+        dontShowButton = new Button(font);
+        dontShowButton.setSprite("sprites/button3.png", 830, 100, 420, 80);
+        dontShowButton.setText("GOT IT !", 1030, 125, 300, 50);
+        dontShowButton.setTextColour(1, 1, 0, 1);
 
-		showClick = new ClickEvent(dontShowButton);
-		showClick.eventType(new ContinueEvent(false));
-		
-		gotchaButton = new Button(font);
-		
-		if(enabled) {
-			gotchaButton.setSprite("sprites/fill.png", 830, 10, 420, 80);
-		} else {
-			gotchaButton.setSprite("sprites/button3.png", 830, 10, 420, 80);
-		}
-		
-		gotchaButton.setText("SHOW HELP AT LAUNCH", 1030, 35, 300, 50);
-		gotchaButton.setTextColour(1, 1, 0, 1);
-		
-		gotchaClick = new ClickEvent(gotchaButton);
-		gotchaClick.eventType(new ContinueEvent(true));
+        helpBackground = new Image("sprites/help.bmp");
+        helpBackground.setPosition(0, 0, 1280, 750);
 
-		EventManager.get().addListener(showClick);
-		EventManager.get().addListener(gotchaClick);
-	}
+        showClick = new ClickEvent(dontShowButton);
+        showClick.eventType(new ContinueEvent(false));
 
-	public void onTouch(MotionEvent e, int x, int y) {
-		if(enabled && e.getAction() == MotionEvent.ACTION_DOWN) {
-			gotchaClick.OnTouch(e, x, y);
-			showClick.OnTouch(e, x, y);
-		}
-	}
+        gotchaButton = new Button(font);
 
-	public void update() {
-		if(enabled) {
-			helpBackground.update();
-			dontShowButton.update();
-			gotchaButton.update();
-		}
-	}
+        if(enabled) {
+            gotchaButton.setSprite("sprites/fill.png", 830, 10, 420, 80);
+        } else {
+            gotchaButton.setSprite("sprites/button3.png", 830, 10, 420, 80);
+        }
 
-	public void enable() {
-		enabled = true;
-	}
-	
-	public void disable() {
-		enabled = false;
-	}
-	
-	public Boolean isActive() {
-		return(enabled);
-	}
+        gotchaButton.setText("SHOW HELP AT LAUNCH", 1030, 35, 300, 50);
+        gotchaButton.setTextColour(1, 1, 0, 1);
 
-	@Override
-	public void render() {
-		if(enabled) {
-			helpBackground.render();
-			dontShowButton.render();
-			gotchaButton.render();
-		}
-	}
+        gotchaClick = new ClickEvent(gotchaButton);
+        gotchaClick.eventType(new ContinueEvent(true));
 
-	private class ContinueEvent implements IEvent {
-		private final Integer TEXTURE_DELAY = 50;
-		private Boolean showHelp;
+        EventManager.get().addListener(showClick);
+        EventManager.get().addListener(gotchaClick);
+    }
 
-		public ContinueEvent(Boolean first) {
-			showHelp = first;	
-		}
+    public void onTouch(MotionEvent e, int x, int y) {
+        if(enabled && e.getAction() == MotionEvent.ACTION_DOWN) {
+            gotchaClick.OnTouch(e, x, y);
+            showClick.OnTouch(e, x, y);
+        }
+    }
 
-		@Override
-		public void onActivate(Object data) {
-			if(!showHelp) {
-				dontShowButton.setTexture("sprites/fill.png");
+    public void update() {
+        if(enabled) {
+            helpBackground.update();
+            dontShowButton.update();
+            gotchaButton.update();
+        }
+    }
 
-				new Timer().schedule(new TimerTask() {
-					@Override
-					public void run() {
-						dontShowButton.setTexture("sprites/button3.png");
-						enabled = false;
-					}
-				}, TEXTURE_DELAY);
-				
-			} 
-			
-			if(showHelp && enabled){				
-				if(gotchaButton.getFilename().compareToIgnoreCase("sprites/fill.png") == 0) {
-					gotchaButton.setTexture("sprites/button3.png");
-					Globals.get().showHelp(false);
-				} else {
-					gotchaButton.setTexture("sprites/fill.png");
-					Globals.get().showHelp(true);
-				}
-			}
-		}
+    public void enable() {
+        enabled = true;
+    }
 
-		@Override
-		public void update() {
-			;
-		}
-	}
-	
+    public void disable() {
+        enabled = false;
+    }
+
+    public Boolean isActive() {
+        return(enabled);
+    }
+
+    @Override
+    public void render() {
+        if(enabled) {
+            helpBackground.render();
+            dontShowButton.render();
+            gotchaButton.render();
+        }
+    }
+
+    private class ContinueEvent implements IEvent {
+        private final Integer TEXTURE_DELAY = 50;
+        private Boolean showHelp;
+
+        public ContinueEvent(Boolean first) {
+            showHelp = first;
+        }
+
+        @Override
+        public void onActivate(Object data) {
+            if(!showHelp) {
+                dontShowButton.setTexture("sprites/fill.png");
+
+                new Timer().schedule(new TimerTask() {
+                    @Override
+                    public void run() {
+                        dontShowButton.setTexture("sprites/button3.png");
+                        enabled = false;
+                    }
+                }, TEXTURE_DELAY);
+
+            }
+
+            if(showHelp && enabled){
+                if(gotchaButton.getFilename().compareToIgnoreCase("sprites/fill.png") == 0) {
+                    gotchaButton.setTexture("sprites/button3.png");
+                    Globals.get().showHelp(false);
+                } else {
+                    gotchaButton.setTexture("sprites/fill.png");
+                    Globals.get().showHelp(true);
+                }
+            }
+        }
+
+        @Override
+        public void update() {
+            ;
+        }
+    }
+
 }
--- app/src/main/java/drawables/TeamObject.java
+++ app/src/main/java/drawables/TeamObject.java
@@ -7,104 +7,104 @@ import objects.Team;
 import java.util.*;
 
 public class TeamObject implements IRenderable {
-	public Label teamScore;
-	public Label name;
-	public Team team;
+    public Label teamScore;
+    public Label name;
+    public Team team;
 
-	private ArrayList<Vector2> positions;
-	private ArrayList<String> strings;
-	private Boolean updateStr;
-	private Integer start_y;
-	private Integer start_x;
+    private ArrayList<Vector2> positions;
+    private ArrayList<String> strings;
+    private Boolean updateStr;
+    private Integer start_y;
+    private Integer start_x;
 
-	public TeamObject(Tournament tournament, int x, int y, int tx, int number) {
-		Font font = Font.get("tiny");
+    public TeamObject(Tournament tournament, int x, int y, int tx, int number) {
+        Font font = Font.get("tiny");
 
-		start_x = tx + 39;
-		start_y = y;
-	
-		positions = new ArrayList<Vector2>();
-		strings = new ArrayList<String>();
-		for(int i = 0; i < 8; i++) {
-			positions.add(new Vector2(i * 30, 0));
-			strings.add(String.valueOf(0));
-		}
+        start_x = tx + 39;
+        start_y = y;
 
-		teamScore = new Label(font);
-		teamScore.Load(start_x, start_y, strings, positions);
-		team = tournament.getTeam(number);
+        positions = new ArrayList<Vector2>();
+        strings = new ArrayList<String>();
+        for(int i = 0; i < 8; i++) {
+            positions.add(new Vector2(i * 30, 0));
+            strings.add(String.valueOf(0));
+        }
 
-		name = new Label(font);
-		name.text(tournament.getTeamName(number));
-		name.load(x, y);
-		name.setColour(1, 1, 0, 1);
-		
-		updateStr = true;
-	}
+        teamScore = new Label(font);
+        teamScore.Load(start_x, start_y, strings, positions);
+        team = tournament.getTeam(number);
 
-	public void updateString(int score, int opponentScore) {
-		if(score != -1 && opponentScore != -1) {
-			team.setGameScore(score, opponentScore);
-		}
-		
-		updateStr = true;
-	}
+        name = new Label(font);
+        name.text(tournament.getTeamName(number));
+        name.load(x, y);
+        name.setColour(1, 1, 0, 1);
 
-	public void updateString(int score, int opponentScore, int previousScore, int previousOpponentScore) {
-		if(score != -1 && opponentScore != -1) {
-			team.updateGameScore(score, opponentScore, previousScore, previousOpponentScore);
-		}
-		
-		updateStr = true;
-	}
+        updateStr = true;
+    }
 
-	public void reset() {
-		teamScore.reset();
-		team.MatchesPlayed--;
-		updateStr = true;
-	}
-	
-	public void setMatchesPlayed(int num) {
-		team.MatchesPlayed = num;
-	}
+    public void updateString(int score, int opponentScore) {
+        if(score != -1 && opponentScore != -1) {
+            team.setGameScore(score, opponentScore);
+        }
 
-	public void update(Integer y) {
-		if(updateStr) {
-			team.MatchesPlayed++;
+        updateStr = true;
+    }
 
-			strings.clear();
-			strings.add(String.valueOf(team.MatchesPlayed));
-			strings.add(String.valueOf(team.MatchesWon));
-			strings.add(String.valueOf(team.MatchesDrawn));
-			strings.add(String.valueOf(team.MatchesLossed));
-			strings.add(String.valueOf(team.GoalsScored));
-			strings.add(String.valueOf(team.GoalsConceeded));
-			strings.add(String.valueOf(team.GoalDifference));
-			strings.add(String.valueOf(team.Points));
-			
-			teamScore.Load(start_x, start_y, strings, positions);
-			updateStr = false;
-		} 
+    public void updateString(int score, int opponentScore, int previousScore, int previousOpponentScore) {
+        if(score != -1 && opponentScore != -1) {
+            team.updateGameScore(score, opponentScore, previousScore, previousOpponentScore);
+        }
 
-		teamScore.translate(teamScore.getPosition().getX(), teamScore.getPosition().getY()+ y);
-		name.translate(name.getPosition().getX(), name.getPosition().getY()+ y);
+        updateStr = true;
+    }
 
-		teamScore.update();
-		name.update();
-	}
+    public void reset() {
+        teamScore.reset();
+        team.MatchesPlayed--;
+        updateStr = true;
+    }
 
-	public void translateLabelScores(float f) {
-		teamScore.translate(teamScore.getPosition().getX(), f);
-		name.translate(name.getPosition().getX(), f);
-	}
+    public void setMatchesPlayed(int num) {
+        team.MatchesPlayed = num;
+    }
 
-	public String getName() {
-		return team.getName();
-	}
+    public void update(Integer y) {
+        if(updateStr) {
+            team.MatchesPlayed++;
 
-	@Override
-	public void render() {
-		teamScore.render();
-		name.render();
-	}
+            strings.clear();
+            strings.add(String.valueOf(team.MatchesPlayed));
+            strings.add(String.valueOf(team.MatchesWon));
+            strings.add(String.valueOf(team.MatchesDrawn));
+            strings.add(String.valueOf(team.MatchesLossed));
+            strings.add(String.valueOf(team.GoalsScored));
+            strings.add(String.valueOf(team.GoalsConceeded));
+            strings.add(String.valueOf(team.GoalDifference));
+            strings.add(String.valueOf(team.Points));
+
+            teamScore.Load(start_x, start_y, strings, positions);
+            updateStr = false;
+        }
+
+        teamScore.translate(teamScore.getPosition().getX(), teamScore.getPosition().getY()+ y);
+        name.translate(name.getPosition().getX(), name.getPosition().getY()+ y);
+
+        teamScore.update();
+        name.update();
+    }
+
+    public void translateLabelScores(float f) {
+        teamScore.translate(teamScore.getPosition().getX(), f);
+        name.translate(name.getPosition().getX(), f);
+    }
+
+    public String getName() {
+        return team.getName();
+    }
+
+    @Override
+    public void render() {
+        teamScore.render();
+        name.render();
+    }
 }
